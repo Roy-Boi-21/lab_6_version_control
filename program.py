99b9e4a45a46bac56676259acd40702dc61474f6
Roy-Boi-21 <royboi21@icloud.com>
2023-10-30 19:28:04 -0400

    My body is ready.

--- program.py
+++ program.py
@@ -4,19 +4,26 @@
 def encode(passcode):
     new_passcode = ""
     for character in passcode:
-        new_passcode += chr(ord(character) + 10)
+        if ord(character) + 3 <= 57:
+            new_passcode += chr(ord(character) + 3)
+        else:
+            new_passcode += chr(ord(character) - 7)
     return new_passcode
 
 
 def decode(passcode):
     new_passcode = ""
     for character in passcode:
-        new_passcode += chr(ord(character) - 10)
+        if ord(character) - 3 >= 48:
+            new_passcode += chr(ord(character) - 3)
+        else:
+            new_passcode += chr(ord(character) + 7)
     return new_passcode
 
 
 def main():
     encoded_passcode = ""
+    valid_passcode = True
 
     while True:
         print("Menu")
@@ -29,8 +36,16 @@ def main():
 
         if choice == "1":
             password = input("Please enter your password to encode: ")
-            encoded_passcode = encode(password)
-            print("Your password has been encoded and stored!")
+            valid_passcode = True
+            for character in password:
+                if not character.isdigit():
+                    valid_passcode = False
+                    break
+            if valid_passcode:
+                encoded_passcode = encode(password)
+                print("Your password has been encoded and stored!")
+            else:
+                print("Invalid passcode!  Only input integers please.")
         elif choice == "2":
             print(f"The encoded password is {encoded_passcode}, and the original password is {decode(encoded_passcode)}.")
         elif choice == "3":
